b8225566c7d42c0de144f18326121a02919b232e
WonderfulLife2121 <nuralif34466@gmail.com>
2022-10-08 15:36:02 +0700

    upload tugas tpa-1

---
+++ script.js
@@ -0,0 +1,12 @@
+// const ul = document.querySelector("ul")
+// const aa = document.querySelector("a")
+
+// ul.addEventListener("click", function(event) {
+//     event.preventDefault();
+
+//     if(event.target.classList.constains("active")) {
+
+//         event.target.classList.add("aktif")
+//     }
+    
+// })
